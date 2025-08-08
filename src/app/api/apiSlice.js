dbe987b1262396f92381ff04998097eea7ee0bd1
JNasri <91850229+JNasri@users.noreply.github.com>
2025-08-09 01:15:02 +0300

    update api url

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://alkutbi-api.onrender.com",
+  baseUrl: "https://alkutbi-backend.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
