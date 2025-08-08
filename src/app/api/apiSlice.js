760a3084f30d5b2b169f347543cb1594c32151a3
JNasri <91850229+JNasri@users.noreply.github.com>
2025-08-09 00:42:54 +0300

    disable devtools and chnage API URL

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:3000",
+  baseUrl: "https://alkutbi-api.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,11 @@ import "../i18n.js";
 import { store } from "./app/store.js";
 import { Provider } from "react-redux";
 import { StrictMode } from "react";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === "production") {
+  disableReactDevTools();
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
